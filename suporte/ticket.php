8ebecac82d1f19e36256d43ab1e938566b52f1ff
LZanata <leonardozana19@gmail.com>
2025-04-26 15:16:05 -0300

    criando envio de ticket

--- suporte/ticket.php
+++ suporte/ticket.php
@@ -12,15 +12,6 @@
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
     <!--BOOTSRAPS FIM-->
-    <style>
-        .hidden {
-            display: none;
-        }
-
-        #contact-form button {
-            color: white;
-        }
-    </style>
 </head>
 
 <body>
@@ -32,21 +23,28 @@
         <!-- Seção de contato -->
         <section id="contact">
             <h2>Contato</h2> <!-- Subtítulo da seção -->
-            <form id="contact-form" action="#" method="post"> <!-- Formulário de contato -->
-                <label for="name">Nome:</label> <!-- Rótulo para o campo nome -->
-                <input type="text" id="name" name="name" required> <!-- Campo de entrada para o nome -->
-                <label for="email">Email:</label> <!-- Rótulo para o campo email -->
-                <input type="email" id="email" name="email" required> <!-- Campo de entrada para o email -->
-                <label for="message">Mensagem:</label> <!-- Rótulo para o campo mensagem -->
-                <textarea id="message" name="message" required></textarea> <!-- Área de texto para a mensagem -->
-                <button type="submit">Enviar</button> <!-- Botão para enviar o formulário -->
+            <form id="contact-form" action="funcoes/processa_ticket.php" method="post">
+                <label for="name">Nome:</label>
+                <input type="text" id="name" name="name" required>
+
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+
+                <label for="subject">Assunto:</label>
+                <input type="text" id="subject" name="subject" required>
+
+                <label for="message">Mensagem:</label>
+                <textarea id="message" name="message" required></textarea>
+
+                <button type="submit" class="btn-submit">Enviar</button>
             </form>
-            <!-- Mensagem de resposta oculta -->
-            <div id="response-message" class="hidden">Sua mensagem foi enviada! O prazo de resposta pode variar de 10 a 30 dias. Caso tenha alguma dúvida mais Urgente, ligue para 11999024689 e fale com nossa Central!</div>
+
+            <div id="response-message" class="hidden success-message">
+                <i class="bi bi-check-circle"></i> Sua mensagem foi enviada!
+            </div>
         </section>
     </main>
     <?php include 'partials/footer.php'; ?> <!-- Inclui o footer -->
-    <script src="scripts.js"></script> <!-- Link para o arquivo de script JavaScript externo -->
 </body>
 
 </html>
