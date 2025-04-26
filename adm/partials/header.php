f0b42de2fdac80bccb9b8dbf9092ac40131787f9
LZanata <leonardozana19@gmail.com>
2025-04-26 19:16:32 -0300

    atualizando header adm

--- adm/partials/header.php
+++ adm/partials/header.php
@@ -1,12 +1,17 @@
 <header>
-    <h1>Painel de Administração</h1>
+    <div class="painel">
+        <h1>Painel de Administração</h1>
+        <nav class="nav-botoes">
+            <a href="../TelaInicial/index.php" class="planos-btn">Área Inicial</a>
+        </nav>
+    </div>
     <nav>
         <ul>
-            <li><a href="index.php" >Dashboard</a></li>
-            <li><a href="cursos.php" >Cursos</a></li>
-            <li><a href="usuarios.php" >Usuários</a></li>
-            <li><a href="suporte.php" >Suporte</a></li>
-            <li><a href="acessibilidade.php" >Acessibilidade</a></li>
+            <li><a href="index.php">Dashboard</a></li>
+            <li><a href="cursos.php">Cursos</a></li>
+            <li><a href="usuarios.php">Usuários</a></li>
+            <li><a href="suporte.php">Suporte</a></li>
+            <li><a href="acessibilidade.php">Acessibilidade</a></li>
         </ul>
     </nav>
 </header>
