a7d4f38e6003ac7406c67f3f774ffe20fae06e2a
LZanata <leonardozana19@gmail.com>
2025-04-28 16:29:12 -0300

    commit

--- suporte/cadastro_login/signup.php
+++ suporte/cadastro_login/signup.php
@@ -72,6 +72,7 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="shortcut icon" href="../../images/logotipocw.png" />
     <link rel="stylesheet" href="signup.css">
     <link rel="stylesheet" href="partials/style.css">
 </head>
